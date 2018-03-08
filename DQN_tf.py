4bc3a6759947dd8897f332a1fc6a657105c51b22
Niann <348258165@qq.com>
2018-03-08 22:56:11 +1100

    basic DQN in progress

---
+++ DQN_tf.py
@@ -0,0 +1,162 @@
+from AirSimClient import *
+from PIL import Image
+
+import numpy as np
+import tensorflow as tf
+
+class ReplayMemory:
+	# memory for experience replay
+	def __init__(self, size, shape, history_length = 4):
+		self._count = 0 # length of current memory
+		self._pos = 0 # number of experiences so far
+		self._memory_size = size
+		self._dim = shape
+		self._history_length = history_length
+		self._states = np.zeros((size,) + sample_shape, dtype=np.float32)
+		self._actions = np.zeros(size, dtype=np.uint8)
+		self._rewards = np.zeros(size, dtype=np.float32)
+		self._terminals = np.zeros(size, dtype=np.float32)
+
+	def __len__(self):
+		return self._count
+	
+	def append(self, state, action, reward, terminal):
+		assert state.shape = self._dim
+		self._states[self._pos] = state
+		self._actions[self._pos] = action
+		self._rewards[self._pos] = reward
+		self._terminals[self._pos] = terminal
+		
+		self._count = max(self._count, self._pos + 1)
+		self._pos = (self._pos + 1) % self._memory_size
+	
+	def sample(self, size):
+		# return indices of sampled experiences
+		count, pos, terminals = self._count-1, self._pos, self._terminals
+		history_len = self._history_length
+		indices = []
+		
+		while len(indices) < size:
+			index = np.random.randint(history_len, count)
+			if index not in indices:
+				if not (index >= pos > index - history_len):
+					if not terminals[(index - history_len):index].any():
+						indices.append(index)
+		return indices
+	
+	def minibatch(self, size):
+		# Generate a minibatch with the number of samples specified by the size parameter
+		indices = self.sample(size)
+		pre_states = np.array([self.get_state(index) for index in indices], dtype=np.float32)
+		post_states = np.array([self.get_state(index + 1) for index in indices], dtype=np.float32)
+		actions = self._actions[indices]
+		rewards = self._rewards[indices]
+		terminals = self._terminals[indices]
+		return pre_states, actions, post_states, rewards, terminals
+
+	def get_state(self, index):
+		# return state with specified index
+		# a state is of shape (history_length, input_shape)
+		if self._count == 0:
+			raise("Empty memory!")
+		index %= self._count
+		if index >= history_length:
+			return self._states[(index - (history_length - 1)):index + 1, ...]
+		else:
+			indexes = np.arange(index - history_length + 1, index + 1)
+			return self._states.take(indexes, mode='wrap', axis=0)
+	
+class History:
+	# Accumulator keeping track of the N previous frames to be used by the agent
+	def __init__(self, shape):
+		self._buffer = np.zeros(shape, dtype=np.float32)
+	
+	def append(self, state):
+        # Append state to the history
+        self._buffer[:-1] = self._buffer[1:]
+        self._buffer[-1] = state
+		
+	def get(self):
+		return self._buffer
+	
+	def reset(self):
+        self._buffer.fill(0)
+
+class DQN_agent:
+	# Deep Q network agent
+	def __init__(self, input_shape, action_num, learning_rate=0.00025, gamma=0.99, epsilon=0.1,
+				 minibatch_size = 32, memory_size=50000, target_update_interval=1000, sess):
+		self.input_shape = input_shape
+		self.action_num = action_num
+		self.gamma = gamma
+		self.minibatch_size = minibatch_size
+		self.epsilon = epsilon
+		self.sess = sess
+		
+		self.replay_memory = ReplayMemory(memory_size, input_shape[1:])
+		self.num_action_taken = 0
+		
+		self.X = tf.placeholder(tf.float32, [None] + input_shape)
+		self.Q_network = build_network("Q_Network", self.X)
+		self.target_network = build_network("target_network", self.X)
+		
+	def build_network(self, scope_name, X):
+		with tf.variable_scope(scope_name):
+		
+			conv1 = tf.layers.conv2d(inputs=X, filters=16, kernel_size=[8,8], strides=[1,4,4,1], padding='same', activation=tf.nn.relu)
+			conv2 = tf.layers.conv2d(inputs=conv1, filters=32, kernel_size=[4,4], strides=[1,2,2,1], padding='same', activation=tf.nn.relu)
+			conv3 = tf.layers.conv2d(inputs=conv2, filters=32, kernel_size=[3,3], strides=[1,1,1,1], padding='same', activation=tf.nn.relu)
+			fc1 = tf.layers.dense(inputs=conv3, units=256, activation=tf.nn.relu)
+			fc2 = tf.layers.dense(inputs=fc1, units=self.action_num)
+		return fc2
+	
+	def update_target_net(self, dest_scope, src_scope):
+		# return tf operations that copy weights to target network
+		ops = []
+		src_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope=src_scope)
+		dest_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope=dest_scope)
+		
+		for src_var, dest_var in zip(src_vars, dest_vars):
+			ops.append(dest_var.assign(src_var.value()))
+		
+		return ops
+	
+	def act(self):
+		pass
+	
+	def observe(self):
+		pass
+	
+	def train(self):
+		pass
+
+def transform_input(responses):
+	# return a numpy array representation of image
+    img1d = np.array(responses[0].image_data_float, dtype=np.float)
+    img1d = 255/np.maximum(np.ones(img1d.size), img1d)
+    img2d = np.reshape(img1d, (responses[0].height, responses[0].width))
+    image = Image.fromarray(img2d)
+    im_final = np.array(image.resize((84, 84)).convert('L')) 
+
+    return im_final
+
+def interpret_action(action):
+    scaling_factor = 0.25
+    if action == 0:
+        quad_offset = (0, 0, 0)
+    elif action == 1:
+        quad_offset = (scaling_factor, 0, 0)
+    elif action == 2:
+        quad_offset = (0, scaling_factor, 0)
+    elif action == 3:
+        quad_offset = (0, 0, scaling_factor)
+    elif action == 4:
+        quad_offset = (-scaling_factor, 0, 0)    
+    elif action == 5:
+        quad_offset = (0, -scaling_factor, 0)
+    elif action == 6:
+        quad_offset = (0, 0, -scaling_factor)
+    return quad_offset
+	
+def compute_reward():
+	pass
